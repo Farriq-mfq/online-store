0ec50170bb3321bf0950606caa1a36839f4f9ff9
unknown <farriqmuwaffaq100@gmail.com>
2022-12-13 16:12:51 -0800

    push

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,11 +6,7 @@ class Home extends BaseController
 {
     public function index()
     {
-        echo "<pre>";
-            // print_r($this->payment->e_money("qris",["transaction_details"=>["gross_amount"=>100,"order_id"=>time()]]));
-            print_r($this->payment->e_money("qris",["transaction_details"=>["gross_amount"=>100,"order_id"=>time()]],1));
-        echo "</pre>";
-        return;
         return view('admin/home_view',add_data("james","/index"));
     }
 }
+
