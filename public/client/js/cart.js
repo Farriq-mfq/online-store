3bd04dc78ee9e08619b367458c5d8020828a7086
unknown <farriqmuwaffaq100@gmail.com>
2022-12-26 09:50:43 +0700

    modify load model

--- public/client/js/cart.js
+++ public/client/js/cart.js
@@ -3,14 +3,21 @@ function addToCart(url, urlload, loadCount, loadTotalPrice) {
     e.preventDefault();
     const $id = $(this).data("id");
     const $qty = $(document).find("#__qty__to__add__cart");
+    const $original = $(this).html();
     $.ajax({
       method: "POST",
       data: {
         id: $id,
         qty: $qty.val(),
       },
+      beforeSend: () => {
+        $(this).text("Waiting...");
+        $(this).attr("disabled", true);
+      },
       url,
       success: (data) => {
+        $(this).html($original);
+        $(this).attr("disabled", false);
         if (data.success) {
           loadCart(urlload, loadCount, loadTotalPrice);
           $.toast({
@@ -31,10 +38,26 @@ function removeCart(url, urlload, loadCount, loadTotalPrice) {
   $(document).on("click", "#__remove__cart__action", function (e) {
     e.preventDefault();
     const $id = $(this).data("id");
+    const $original = $(this).parent().html();
     $.ajax({
       method: "POST",
       data: {
         id: $id,
+      },
+      beforeSend: () => {
+        const $div = $("<div>");
+        $div.css({
+          position: "absolute",
+          top: "0",
+          right: "0",
+          left: "0",
+          bottom: "0",
+          "background-color": "#000",
+          opacity: "0.2",
+          "z-index": "999",
+        });
+        $(this).parent().css("position", "relative");
+        $(this).parent().append($div);
       },
       url,
       success: (data) => {
@@ -51,13 +74,18 @@ function removeCart(url, urlload, loadCount, loadTotalPrice) {
           });
         }
       },
+      error: () => {
+        $(this).parent().append($original);
+      },
     });
   });
 }
 function loadCart(loadUrl, loadCount, loadTotalPrice) {
   const $wrapper__ = $(document).find("#load__cart____");
   const $wrapper__cart__count__ = $(document).find("#load__cart__count");
-  const $wrapper__cart__count__mobile = $(document).find("#load__cart__count__mobile");
+  const $wrapper__cart__count__mobile = $(document).find(
+    "#load__cart__count__mobile"
+  );
   const $wrapper__total__price__ = $(document).find("#load__total__price");
   $wrapper__.load(loadUrl);
   $wrapper__cart__count__.load(loadCount);
--- public/client/js/client.js
+++ public/client/js/client.js
@@ -1,71 +1,75 @@
-function clickModal(urlcall,urlload) {
-    $(document).on("click", "#show_detail_product", function(e) {
-        e.preventDefault();
-        const $modal = $(document).find("#quickModal")
-        const $id = $(this).data("id");
-        const $image = $(document).find("#product_image_modal");
-        const $slider_image = $(document).find("#slider_image_modal");
-        $.get({
-            url: urlcall,
-            data: {
-                id: $id,
-            },
-            beforeSend: () => {
-                $image.slick("destroy");
-                $slider_image.slick("destroy");
-                $slider_image.children().remove()
-                $image.children().remove()
-                $(this).css("pointer-events", "none")
-            },
-            success: (data) => {
-                $(this).css("pointer-events", "auto")
-                if (data != null) {
-                    for (let i = 0; i < data.product_images.length; i++) {
-                        const images = data.product_images[i];
-                        $image.append(`<div class="single-slide">
+function clickModal(urlcall, urlload) {
+  $(document).on("click", "#show_detail_product", function (e) {
+    e.preventDefault();
+    const $modal = $(document).find("#quickModal");
+    const $id = $(this).data("id");
+    const $image = $(document).find("#product_image_modal");
+    const $slider_image = $(document).find("#slider_image_modal");
+    $.get({
+      url: urlcall,
+      data: {
+        id: $id,
+      },
+      beforeSend: () => {
+        $image.slick("destroy");
+        $slider_image.slick("destroy");
+        $slider_image.children().remove();
+        $image.children().remove();
+        $(this).css("pointer-events", "none");
+      },
+      success: (data) => {
+        $(this).css("pointer-events", "auto");
+        if (data != null) {
+          for (let i = 0; i < data.product_images.length; i++) {
+            const images = data.product_images[i];
+            $image.append(`<div class="single-slide">
                         <img src="${images.image}" alt="Product image">
-                        </div>`)
-                    }
+                        </div>`);
+          }
 
-                    for (let i = 0; i < data.product_images.length; i++) {
-                        const images = data.product_images[i];
-                        $slider_image.append(`<div class="single-slide">
+          for (let i = 0; i < data.product_images.length; i++) {
+            const images = data.product_images[i];
+            $slider_image
+              .append(
+                `<div class="single-slide">
                                 <img src="${images.image}" alt="Product image">
-                            </div>`).fadeIn(400)
-                    }
-                    setTimeout(() => {
-                        $image.slick({
-                            "slidesToShow": 1,
-                            "arrows": false,
-                            "fade": true,
-                            "draggable": false,
-                            "swipe": false,
-                            "asNavFor": ".product-slider-nav"
-                        })
-                        $slider_image.slick({
-                            "infinite": true,
-                            "autoplay": true,
-                            "autoplaySpeed": 8000,
-                            "slidesToShow": 4,
-                            "arrows": true,
-                            "prevArrow": {
-                                "buttonClass": "slick-prev",
-                                "iconClass": "fa fa-chevron-left"
-                            },
-                            "nextArrow": {
-                                "buttonClass": "slick-next",
-                                "iconClass": "fa fa-chevron-right"
-                            },
-                            "asNavFor": ".product-details-slider",
-                            "focusOnSelect": true
-                        })
-                    }, 200);
-                    $("#load_product").load(urlload, `id=${$id}`)
-                    $modal.modal("show");
-                }
-            }
-        });
+                            </div>`
+              )
+              .fadeIn(400);
+          }
 
-    })
+          $("#load_product").load(urlload, `id=${$id}`, function () {
+            setTimeout(() => {
+              $image.slick({
+                slidesToShow: 1,
+                arrows: false,
+                fade: true,
+                draggable: false,
+                swipe: false,
+                asNavFor: ".product-slider-nav",
+              });
+              $slider_image.slick({
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 8000,
+                slidesToShow: 4,
+                arrows: true,
+                prevArrow: {
+                  buttonClass: "slick-prev",
+                  iconClass: "fa fa-chevron-left",
+                },
+                nextArrow: {
+                  buttonClass: "slick-next",
+                  iconClass: "fa fa-chevron-right",
+                },
+                asNavFor: ".product-details-slider",
+                focusOnSelect: true,
+              });
+            }, 200);
+            $modal.modal("show");
+          });
+        }
+      },
+    });
+  });
 }
-
